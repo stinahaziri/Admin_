9a1df128106795438b1f5a193f3f0face87fd9ba
stinahaziri <stinna.haziri@gmail.com>
2025-06-25 17:02:42 +0200

    s

--- obj/Debug/net9.0/Admin.AssemblyInfo.cs
+++ obj/Debug/net9.0/Admin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42876a701d39b68d1b85b34ba1b68bc23e17e0c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cacb74674a0b32bee8cba0590e1756b0cd92d3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
